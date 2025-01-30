6ea051e1077b4c2838fffbefd2430d3d12a45257
paulgithub.8 <Ot_moroz_ochek.8@mail.ru>
2025-01-30 17:15:08 +0500

    решено без задания со звёздочкой

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
-
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
-
+        for(int i = array.length - 1; i >= 0; i--){
+            System.out.println(array[i]);
+        }
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
 
